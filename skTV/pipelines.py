8fee322d64ebbe8b2f53039aaa7bbb3cf5aaf388
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-10-03 09:19:03 +0700

    add configuration to save into sqlite3 database

--- skTV/pipelines.py
+++ skTV/pipelines.py
@@ -4,8 +4,37 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import sqlite3
 
 class SktvPipeline(object):
+
+	def __init__(self):
+		self.create_connection()
+		self.create_table()
+
+	def create_connection(self):
+		self.conn = sqlite3.connect("sktv.db")
+		self.curr = self.conn
+
+	def create_table(self):
+		self.cursor.execute("""DROP TABLE IF EXISTS ktv_tb""")
+		self.cursor.execute("""create table ktv_tb(
+						title text,
+						tv_network text,
+						mont_release text,
+						rating text
+					)""")
+
     def process_item(self, item, spider):
+    	self.store_db(item)
+    	print("Pipline" + item['title'][0])
         return item
+
+    def store_db(self):
+    	self.curr.execute("""insert into ktv_tb values (?,?,?)(
+    			item['title'][0],
+    			item['tv_network'][0],
+    			item['mont_release'][0],
+    			item['rating'][0],
+    		)""")
+    	self.conn.commit()
